6691837d1cfa69a03be205452d249fcd96d3889a
Erick Fonseca <erickrfonseca@gmail.com>
2015-08-19 01:16:17 -0300

    added tokenizer code

--- src/utils.py
+++ src/utils.py
@@ -6,7 +6,7 @@ Utility functions.
 
 import re
 
-import nlpnet
+from nltk.tokenize.regexp import RegexpTokenizer
 
 def detokenize(tokens):
     '''
@@ -24,5 +24,23 @@ def tokenize_sentence(text, preprocess=True):
     if preprocess:
         text = re.sub(r'\d', '9', text.lower())
     
-    return nlpnet.utils.tokenize_sentence_pt(text)
+    tokenizer_regexp = ur'''(?ux)
+    ([^\W\d_]\.)+|                # one letter abbreviations, e.g. E.U.A.
+    \d{1,3}(\.\d{3})*(,\d+)|      # numbers in format 999.999.999,99999
+    \d{1,3}(,\d{3})*(\.\d+)|      # numbers in format 999,999,999.99999
+    \d+:\d+|                      # time and proportions
+    \d+([-\\/]\d+)*|              # dates. 12/03/2012 12-03-2012
+    [DSds][Rr][Aa]?\.|            # common abbreviations such as dr., sr., sra., dra.
+    [Mm]\.?[Ss][Cc]\.?|           # M.Sc. with or without capitalization and dots
+    [Pp][Hh]\.?[Dd]\.?|           # Same for Ph.D.
+    [^\W\d_]{1,2}\$|              # currency
+    (?:(?<=\s)|^)[\#@]\w*[A-Za-z_]+\w*|  # Hashtags and twitter user names
+    -[^\W\d_]+|                   # clitic pronouns with leading hyphen
+    \w+([-']\w+)*|                # words with hyphens or apostrophes, e.g. não-verbal, McDonald's
+    -+|                           # any sequence of dashes
+    \.{3,}|                       # ellipsis or sequences of dots
+    \S                            # any non-space character
+    '''
+    tokenizer = RegexpTokenizer(tokenizer_regexp)
     
+    return tokenizer.tokenize(text)
